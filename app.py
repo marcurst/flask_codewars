ae467aaa5cf9e02684c521f183a33553c8af1afb
Lusca Andrei <oscicen@gmail.com>
2020-04-04 18:05:42 +0300

    first ex

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, request
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/", methods=['GET'])
 def index():
-    return "Hello World"
+    return "Codewars API"
+
+@app.route("/multiply/", methods=['POST'])
+def multiply():
+    data = request.get_json()
+    res = str(data['a'] * data['b'])
+    return res, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
-
